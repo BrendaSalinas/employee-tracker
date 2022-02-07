e6eb2e7327119ca12f17a36a371aece5f73659a0
Brenda Salinas <brenda.salinas2128@gmail.com>
2022-02-06 19:57:42 -0600

    add department function

--- app.js
+++ app.js
@@ -1,50 +1,100 @@
 const inquirer = require('inquirer');
 const { prompt } = require('inquirer');
+const { clearLine } = require('inquirer/lib/utils/readline');
 const db = require('./db/connection');
 
 //view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-prompt([
-    {
-        type: 'list',
-        name: 'options',
-        message: 'What would you like to do? ',
-        choices: [
-            'view all departments', 
-            'view all roles', 
-            'view all employees', 
-            'add a department', 
-            'add a role', 
-            'add an employee', 
-            'update an employee role'
-        ]
-    }
-]).then(function(choice){
-    switch(choice.options){
-        case 'view all departments':
-            viewDepartments();
-            break;
-        case 'view all roles':
-            viewRoles();
-            break;
-        case 'view all employees':
-            viewEmployees();
-            break;
-        case 'add a department':
-            addDepartment();
-            break;
-        case 'add a role':
-            addRole();
-            break;
-        case 'add an employee':
-            addEmployee();
-            break;
-        case 'update an employee role':
-            updateRole();
-            break;
-        default:
-            console.log('default');
-    }
-});
+function callback() {
+    prompt([
+        {
+            type: 'list',
+            name: 'options',
+            message: 'What would you like to do? ',
+            choices: [
+                'view all departments', 
+                'view all roles', 
+                'view all employees', 
+                'add a department', 
+                'add a role', 
+                'add an employee', 
+                'update an employee role'
+            ]
+        }
+    ]).then(function(choice){
+        switch(choice.options){
+            case 'view all departments':
+                viewDepartments();
+                break;
+            case 'view all roles':
+                viewRoles();
+                break;
+            case 'view all employees':
+                viewEmployees();
+                break;
+            case 'add a department':
+                addDepartment();
+                break;
+            case 'add a role':
+                addRole();
+                break;
+            case 'add an employee':
+                addEmployee();
+                break;
+            case 'update an employee role':
+                updateRole();
+                break;
+            default:
+                console.log('default');
+        }
+    });
+};
+
+// function viewDepartments() {
+//     db.query('SELECT * FROM department', function(err, res){
+//         if(err) throw err;
+
+//         inquirer.prompt([
+//             {
+//                 name: 'choice',
+//                 type: 'rawlist',
+//                 choices: function() {
+//                     let choice = [];
+//                     for (i =0; i< res.length; i++) {
+//                         choice.push(res[i].name);
+//                     }
+//                     return choice;
+//                 },
+//                 message: 'Choose department to view'
+//             }
+//         ]).then(function(theChoice) {
+//             db.query('')
+//         }
+//     })
+// }
+
+
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: 'department',
+            type: 'input',
+            message: 'Add the name of the department'
+        }
+    ]).then(function(ans) {
+        db.query(
+            'INSERT INTO department VALUES (default, ?)',
+            [ans.department],
+            function(err){
+                if(err) throw err;
+
+                console.log(`Departments have been updated ${ans.department}`)
+                callback();
+            }
+        )
+    })
+}
+
+
 function addRole() {
     inquirer.prompt([
         {
@@ -62,20 +112,21 @@ function addRole() {
             type: 'number',
             message: 'Please enter department id: '
         }
-    ]).then(answer => {
+    ]).then(ans => {
         db.query(
             'INSERT INTO role SET ?',
             {
-                title: answer.role,
-                salary: answer.salary,
-                department_id: answer.department_id
+                title: ans.role,
+                salary: ans.salary,
+                department_id: ans.department_id
             },
             function (err) {
-                if (err)
-                    throw err;
-
-                console.log('Employee role updated with' + answer.role);
+                if (err) throw err;
+                console.log(`Employee role updated with ${ans.role}`);
+                callback();
             }
         );
     });
-}
+};
+
+callback();
